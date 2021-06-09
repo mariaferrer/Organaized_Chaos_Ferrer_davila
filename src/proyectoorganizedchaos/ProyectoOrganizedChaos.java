4f328aeadacd0aac63238a0de64e3bbc5ae8bf71
mkferrerteran <mkferrerteran@localhost>
2021-06-09 14:54:36 -0400

    proyecto 1- Organized Chaos

--- src/proyectoorganizedchaos/ProyectoOrganizedChaos.java
+++ src/proyectoorganizedchaos/ProyectoOrganizedChaos.java
@@ -16,7 +16,7 @@ public class ProyectoOrganizedChaos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        System.out.println("hello");
     }
     
 }
